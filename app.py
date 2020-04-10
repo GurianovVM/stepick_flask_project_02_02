3e4a118809e1adb1c6aab0875e46c324aefae93f
gvm <79202946014@yandex.ru>
2020-04-10 19:02:32 +0300

    done

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
 from flask_migrate import Migrate
 import json
-import random
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, RadioField
+from wtforms.validators import InputRequired, Length
 
 with open('./static/goals.json', 'r') as f:
     goals = json.load(f)
-
+'''
 with open('./static/teachers.json', 'r') as f:
     teachers = json.load(f)
-
+'''
 week = {'mon': 'Понедельник', 'tue': 'Вторник', 'wed': 'Среда', 'thu': 'Четверг', 'fri': 'Пятница', 'sat': 'Суббота',
         'sun': 'Воскресенье'}
+times = {'time_1': '1-2', 'time_2': '3-5', 'time_3': '5-7', 'time_4': '7-10'}
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+# Модель учителя в бд
 class Teacher(db.Model):
     __tablename__ = 'teachers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    about = db.Column(db.String(1000), nullable=False)
+    about = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     picture = db.Column(db.String(550), nullable=False)
     price = db.Column(db.Integer, nullable=False)
@@ -33,17 +39,19 @@ class Teacher(db.Model):
     reverse = db.relationship('Reserve')
 
 
+# Модель запроса на выбор времени
 class Reserve(db.Model):
     __tablename__ = 'reserves'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.Integer, nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
     day = db.Column(db.String(3), nullable=False)
     time = db.Column(db.Integer, nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
     teacher = db.relationship('Teacher')
 
 
+# Модель запроса на подбор преподавателя
 class Select(db.Model):
     __tablename__ = 'selections'
     id = db.Column(db.Integer, primary_key=True)
@@ -53,22 +61,37 @@ class Select(db.Model):
     time_learning = db.Column(db.String(20), nullable=False)
 
 
+# Модель формы резерва времени
+class ReserveForm(FlaskForm):
+    name = StringField('Вас зовут', [InputRequired(), Length(min=3, max=100, message="от 3 до 100 символов")])
+    phone = StringField('Ваш телефон', [InputRequired(), Length(min=4, max=15, message="от 4 до 15 цифр")])
+    submit = SubmitField('Записаться на пробный урок')
+
+
+# Модель на запрос преподователя
+class SelectForm(FlaskForm):
+    name = StringField('Вас зовут', [InputRequired(), Length(min=3, max=100, message="от 3 до 100 символов")])
+    phone = StringField('Ваш телефон', [InputRequired(), Length(min=4, max=15, message="от 4 до 15 цифр")])
+    goal = RadioField('Какая цель занятий?', default="work")
+    limit_time = RadioField('Сколько времени есть?', default="time_3")
+    submit = SubmitField('Найдите мне преподователя')
+
+
+# заполнение таблицы учителей
 def db_teachers():
     for t in teachers:
         create_teacher = Teacher(name=t['name'], about=t['about'], rating=t['rating'], picture=t['picture'],
-                                 price=t['price'], goal=str(t['goals']), free=str(t['free']))
+                                 price=t['price'], goal=", ".join(t['goals']), free=str(json.dumps(t['free'])))
         db.session.add(create_teacher)
     db.session.commit()
 
-# db_teachers() #заполнение таблицы учителей
+
+# db_teachers()
 
 
 @app.route('/')
 def render_main():
-    temp_list = sorted(random.sample(range(1, 11), 6))
-    teacher = []
-    for i in temp_list:
-        teacher.append(teachers[i])
+    teacher = db.session.query(Teacher).order_by(func.random()).limit(6)
     temp_goal = {}
     for i in goals:
         temp_goal[i] = goals[i]
@@ -81,13 +104,7 @@ def render_main():
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
-    goal_teacher = []
-    for teacher in teachers:
-        temp_list = teacher['goals']
-        for i in temp_list:
-            if i == goal:
-                goal_teacher.append(teacher)
-
+    goal_teacher = db.session.query(Teacher).filter(Teacher.goal.like('%' + goal + '%')).order_by(Teacher.rating).all()
     for i in goals:
         if i == goal:
             goal = goals[i]
@@ -96,159 +113,60 @@ def render_goal(goal):
 
 @app.route('/profiles/<id_teacher>/')
 def render_profiles(id_teacher):
-    teacher = {}
-    for i in range(len(teachers)):
-        if int(id_teacher) == teachers[i]['id']:
-            teacher = teachers[i]
+    temp = db.session.query(Teacher).get_or_404(id_teacher)
+    teacher = {"id": temp.id, "name": temp.name, "about": temp.about, "rating": temp.rating, "picture": temp.picture,
+               "price": temp.price, "free": json.loads(temp.free)}
     return render_template('profile.html', teacher=teacher, week=week, goals=goals)
 
 
-@app.route('/request/')
+@app.route('/request/', methods=['GET', 'POST'])
 def render_request():
-    form = """<form action="/request_done/" class="card mb-5" method="POST">
-        <div class="card-body text-center pt-5">
-          <h1 class="h3 card-title mt-4 mb-2">Подбор преподавателя</h1>
-          <p class="px-5">Напишите, чего вам нужно и&nbsp;мы&nbsp;подберем отличных&nbsp;ребят</p>
-        </div>
-        <hr>
-        <div class="card-body mx-3">
-          <div class="row">
-            <div class="col">
-              <p>Какая цель занятий?</p>
-              <div class="form-check ">
-                <input type="radio" class="form-check-input" name="goal" value="travel" id="goal1" checked>
-                <label class="form-check-label" for="goal1">
-                  Для путешествий
-                </label> 
-              </div>
-              <div class="form-check ">
-                <input type="radio" class="form-check-input" name="goal" value="learn" id="goal2">
-                <label class="form-check-label" for="goal2">
-                  Для школы
-                </label>
-              </div>
-              <div class="form-check ">
-                <input type="radio" class="form-check-input" name="goal" value="work" id="goal3">
-                <label class="form-check-label" for="goal2">
-                  Для работы
-                </label>
-              </div>
-              <div class="form-check ">
-                <input type="radio" class="form-check-input" name="goal" value="move" id="goal4">
-                <label class="form-check-label" for="goal2">
-                  Для переезда
-                </label>
-              </div>
-            </div>
-            <div class="col">
-              <p>Сколько времени есть?</p>
-              <div class="form-check">
-                <input type="radio" class="form-check-input" name="time" value="1-2" id="time1">
-                <label class="form-check-label" for="time1">
-                  1-2 часа в&nbsp;неделю
-                </label> 
-              </div>
-              <div class="form-check">
-                <input type="radio" class="form-check-input" name="time" value="3-5" id="time2">
-                <label class="form-check-label" for="time2">
-                  3-5 часов в&nbsp;неделю
-                </label>
-              </div>
-              <div class="form-check">
-                <input type="radio" class="form-check-input" name="time" value="5-7" id="time3" checked>
-                <label class="form-check-label" for="time3">
-                  5-7 часов в&nbsp;неделю
-                </label> 
-              </div>
-              <div class="form-check">
-                <input type="radio" class="form-check-input" name="time" value="7-10" id="time4">
-                <label class="form-check-label" for="time4">
-                  7-10 часов в&nbsp;неделю
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr>
-        <div class="card-body mx-3">
-          <label class="mb-1 mt-2">Вас зовут</label>
-          <input class="form-control" type="text" placeholder="ФИО" name = "request_name">
-          <label class="mb-1 mt-2">Ваш телефон</label>
-          <input class="form-control" type="text" placeholder="+7. . ." name = "request_phone">
-          <input type="submit" class="btn btn-primary mt-4 mb-2" value="Найдите мне преподавателя">
-        </div>
-      </form>"""
-    return render_template('request.html')
+    form = SelectForm()
+    choice_goal = []
+    for i in goals:
+        choice_goal.append(tuple([i, goals[i]]))
+    form.goal.choices = choice_goal
+    choice_time = []
+    for i in times:
+        choice_time.append(tuple([i, times[i]]))
+    form.limit_time.choices = choice_time
+
+    if form.validate_on_submit():
+        select_order = Select(name=form.name.data, phone=form.phone.data, goal=form.goal.data,
+                              time_learning=form.limit_time.data)
+        db.session.add(select_order)
+        db.session.commit()
+        return render_template('request_done.html', name=form.name.data, phone=form.phone.data,
+                               time=times[form.limit_time.data], goal=goals[form.goal.data])
+    else:
+        return render_template('request.html', form=form)
 
 
-@app.route('/request_done/', methods=['POST'])
-def render_request_done():
-    requests = {'client_goal': request.form.get('goal'),
-                'client_time': request.form.get('time'),
-                'client_name': request.form.get('request_name'),
-                'client_phone': request.form.get('request_phone')}
-    with open('request.jsonlines', 'a', encoding='utf-8') as f:
-        json.dump(requests, f, ensure_ascii=False)
-        f.write('\n')
-    return render_template('request_done.html')
-
-
-@app.route('/booking/<id_teacher>/<day>/<time>/')
+@app.route('/booking/<id_teacher>/<day>/<time>/', methods=['GET', 'POST'])
 def render_booking(id_teacher, day, time):
-    teacher = {}
-    for i in range(len(teachers)):
-        if int(id_teacher) == teachers[i]['id']:
-            teacher = teachers[i]
-    form = """<form action="booking_done" class="card mb-3" method="POST">
-          <div class="card-body text-center pt-5">
-            <img src="{{ teacher.picture }}" class="mb-3" width="95" alt="">
-            <h2 class="h5 card-title mt-2 mb-2">{{teacher.name}}</h2>
-            <p class="my-1">Запись на пробный урок</p>
-            <p class="my-1">{%for i,j in week.items()%}{%if day == i%}{{j}}{%endif%}{%endfor%}, {{hour}}:00</p>
-          </div>
-          <hr />
-          <div class="card-body mx-3">
-              <div class="row">
-                  <input class="form-control" type="hidden" name="clientWeekday" value="{{day}}">
-                  <input class="form-control" type="hidden" name="clientTime" value="{{hour}}">
-                  <input class="form-control" type="hidden" name="clientTeacher" value="{{teacher.id}}">
-              </div>
+    teacher_query = db.session.query(Teacher).get_or_404(id_teacher)
+    teacher = {"id": teacher_query.id, "name": teacher_query.name, "picture": teacher_query.picture}
+    form = ReserveForm()
+    client_day = ''
+    for week_day in week:
+        if day == week_day:
+            client_day = week[week_day]
 
-            <label class="mb-1 mt-2" for="clientName">Вас зовут</label>
-            <input class="form-control" type="text" name="clientName" id="clientName">
-
-            <label class="mb-1 mt-2" for="clientPhone">Ваш телефон</label>
-            <input class="form-control" type="tel"  name="clientPhone" id="clientPhone">
-
-            <input type="submit" class="btn btn-primary btn-block mt-4" value="Записаться на пробный урок">
-
-          </div>
-        </form>"""
-    return render_template('booking.html', teacher=teacher, day=day, hour=time, week=week)
-
-
-@app.route('/booking_done/', methods=['POST'])
-def form_booking():
-    booking = {'client_day': request.form.get('clientWeekday'),
-               'client_time': request.form.get('clientTime'),
-               'teacher_id': request.form.get('clientTeacher'),
-               'client_name': request.form.get('clientName'),
-               'client_phone': request.form.get('clientPhone')}
-    with open('booking.jsonlines', 'a', encoding='utf-8') as f:
-        json.dump(booking, f)
-        f.write('\n')
-    client_week_day = ''
-    for i in week:
-        if booking['client_day'] == i:
-            client_week_day = week[i]
-    return render_template('booking_done.html', name=booking['client_name'], day=client_week_day,
-                           hour=booking['client_time'], phone=booking['client_phone'])
+    if form.validate_on_submit():
+        reserve_order = Reserve(name=form.name.data, phone=form.phone.data, day=day, time=time, teacher_id=id_teacher)
+        db.session.add(reserve_order)
+        db.session.commit()
+        return render_template('booking_done.html', name=reserve_order.name, client_day=client_day,
+                               hour=reserve_order.time, phone=reserve_order.phone)
+    else:
+        return render_template('booking.html', teacher=teacher, day=day, client_day=client_day, time=time, form=form,
+                               id=id_teacher)
 
 
 @app.route('/all/')
 def render_all_profiles():
-    return render_template('all_profile.html', teachers=teachers)
+    return render_template('all_profile.html', teachers=db.session.query(Teacher).all())
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
